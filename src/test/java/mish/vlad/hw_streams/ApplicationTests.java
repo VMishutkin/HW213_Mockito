0f740ee02571fac0fa111497ce68793a30b5cbca
VMishutkin <vladmish@list.ru>
2022-09-09 02:00:35 +0700

    Initial commit

---
+++ src/test/java/mish/vlad/hw_streams/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mish.vlad.hw_streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
